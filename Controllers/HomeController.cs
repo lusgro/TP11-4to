241f90c20a0f8c37edc176ef52c2ec2480732d1f
Lucas Rosenbaum <lucasurielrosenbaum@gmail.com>
2023-10-26 14:57:42 -0300

    Prueba API

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SpotifyAPI.Web;
 
 namespace TP11.Controllers;
 
@@ -6,6 +7,20 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        Main().Wait();
         return View();
+    }
+
+    static async Task Main()
+    {
+        var config = SpotifyClientConfig.CreateDefault();
+
+        var request = new ClientCredentialsRequest("7b5afc5ef1744921b98041719f50d5a1", "de94ef49b7674ab59a1de9ba6db84900");
+        var response = await new OAuthClient(config).RequestToken(request);
+
+        var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+
+        var track = await spotify.Tracks.Get("05mDaV9Vb3wrzjF6OPZnhq");
+        Console.WriteLine(track.Name);
     }
 }
