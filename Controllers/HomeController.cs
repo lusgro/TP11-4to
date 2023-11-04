85c518cfc3cc63b0008208840d355b6916e7b4e2
lusgro <lucasurielrosenbaum@gmail.com>
2023-11-03 23:56:02 -0300

    Creacion Login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SpotifyAPI.Web;
+using TP11.Models;
 
 namespace TP11.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
-    {
-        Main().Wait();
+    [HttpPost]
+    public IActionResult VerificarLogin(string user, string password) {
+        Usuario usuario = BD.Login(user, password);
+        if (usuario != null) {
+            ViewBag.Usuario = usuario;
+            return View("Bienvenido");
+        } else {
+            ViewBag.Usuario = usuario;
+            return View("Bienvenido");
+        }
+    }
+
+    public IActionResult Registro() {
         return View();
     }
 
-    static async Task Main()
-    {
-        var config = SpotifyClientConfig.CreateDefault();
+    [HttpPost]
+    public IActionResult RegistrarUsuario(Usuario usuario) {
+        BD.RegistrarUsuario(usuario);
+        return RedirectToAction("Bienvenido", new { user = usuario });
+    }
 
-        var request = new ClientCredentialsRequest("7b5afc5ef1744921b98041719f50d5a1", "de94ef49b7674ab59a1de9ba6db84900");
-        var response = await new OAuthClient(config).RequestToken(request);
+    public IActionResult Olvide() {
+        return View();
+    }
 
-        var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
-
-        var track = await spotify.Tracks.Get("05mDaV9Vb3wrzjF6OPZnhq");
-        Console.WriteLine(track.Name);
+    [HttpPost]
+    public IActionResult RecuperarContraseña(string email, string preguntaSeguridad) {
+        string password = BD.RecuperarContraseña(email, preguntaSeguridad);
+        if (password != null) {
+            ViewBag.Resultado = $"La contraseña es {password}";
+            return View("Olvide");
+        } else {
+            ViewBag.Resultado = "No se encontró el usuario";
+            return View("Olvide");
+        }
     }
 }
---
+++ Models/Usuario.cs
@@ -0,0 +1,7 @@
+namespace TP11.Models;
+public class Usuario {
+    public string username { get; set; }
+    public string password { get; set; }
+    public string email { get; set; }
+    public string preguntaSeguridad { get; set; }
+}
