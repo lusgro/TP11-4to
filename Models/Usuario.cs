f88c100ef3a82f529cdf92c3ef7517cb3da4915a
Rama Kwon <116977233+Rameik@users.noreply.github.com>
2023-11-07 08:23:12 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,5 +3,4 @@ public class Usuario {
     public string Usuario { get; set; }
     public string Contraseña { get; set; }
     public string Email { get; set; }
-    public string PreguntaSeguridad { get; set; }
 }
