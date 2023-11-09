151992f7d81427b2b15bfff5460ee6080a7c735c
Rameik <ramakwon07@gmail.com>
2023-11-09 17:54:54 -0300

    Creacion de NavBar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -58,3 +58,17 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.menu-desplegado');
+
+toggleBtn.onclick = function(){
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+
+
+    toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars';
+}
