1c813b6f5b8e6c00d3c5afd18dd9fcb18934cb07
FRANCO IAIR BRONSZTEIN CASTELLAN <47344052@ort>
2023-11-09 14:45:49 -0300

    Perfil View Terminada

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,7 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public IActionResult Comunidad() {
+    public IActionResult Comunidad(int id) {
         return View();
     }
 
