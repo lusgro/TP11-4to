418ac7695a7316d5df7a32e92826a391d2f58a48
FRANCO IAIR BRONSZTEIN CASTELLAN <47344052@ort>
2023-11-16 13:40:48 -0300

    inserts.sql

---
+++ wwwroot/data/Inserts.sql
@@ -0,0 +1,37 @@
+-- Artistas --
+
+insert [dbo].[Artistas] ([ID_Artista],[Nombre],[Descripcion],[Oyentes],[FotoPerfil]) Values ('','','','','','')
+
+
+-- Albumes --
+
+insert [dbo].[Albumes] ([ID_Album],[ID_Artista],[Nombre],[CantCanciones],[CantReproducciones],[DuracionTotal],[FotoPortada]) Values ('','','', , , '')
+
+
+-- Canciones --
+
+insert [dbo].[Canciones] ([ID_Cancion],[ID_Album],[Nombre],[Duracion],[FechaPublicacion],[CantReproducciones]) Values ('','','','','',)
+
+
+-- Comunidades --
+
+
+insert [dbo].[Comunidades] ([ID_Comunidad],[ID_Artista],[Nombre],[FotoPerfil]) Values ('','','','')
+
+
+-- Mensajes --
+
+insert [dbo].[Mensajes]([ID_Mensaje],[ID_Comunidad],[ID_Usuario],[Contenido],[Fecha]) Values ('','','','','','')
+
+
+
+-- Usuarios --
+
+
+insert [dbo].[Usuarios] ([ID_Usuario],[Username],[Contraseña],[Email],[FotoPerfil]) Values ('','','','','','')
+
+
+-- UsuariosXComunidades --
+
+
+insert [dbo].[UsuariosXComunidades] ([ID_Comunidad],[ID_Usuario]) Values ('','')
